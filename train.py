692ba78f7d7c28b7ac4954541b9db41b509bfb0f
Your Name <you@example.com>
2025-11-03 09:25:10 +0100

    Workflow with cml 2

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 # Entrainement
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = RandomForestClassifier(n_estimators=50, random_state=42)
 model.fit(X_train, y_train)
 
 # Evaluation
